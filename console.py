5ed9c70828d246a478aa6c2e76bec4d2ee4cdca5
CoderYounes <younesanas2021@gmail.com>
2023-12-18 09:56:42 +0100

    Try the first update

--- console.py
+++ console.py
@@ -156,6 +156,7 @@ class HBNBCommand(cmd.Cmd):
 
                 kwargs[key] = val
         new_instance = HBNBCommand.classes[class_name](**kwargs)
+        storage.new(new_instance)
         storage.save()
         print(new_instance.id)
 
@@ -220,7 +221,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -352,6 +353,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
