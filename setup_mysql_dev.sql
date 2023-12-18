e9a48cbe3cbccff3c2291fd41926af30f176bb26
CoderYounes <younesanas2021@gmail.com>
2023-12-18 12:04:34 +0100

    Setup mysql database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- SQL Script Prepare the MySQL server
+-- The Script Create New_DB, new_User, set all priviléges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES on hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
