cf68f4ec575381cc0b5b574406c85120621c856e
CoderYounes <younesanas2021@gmail.com>
2023-12-18 17:53:47 +0100

    add delete method & update the all method & fix style issue

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            dicct = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or value.__class__.__name__:
+                    dicct[key] = value
+            return dicct
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +51,11 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects.pop(key, None)
