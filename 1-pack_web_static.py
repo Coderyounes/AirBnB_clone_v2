e0d8ccae69196d3c68ce2278b8d261633b97aa0a
CoderYounes <younesanas2021@gmail.com>
2024-01-11 20:09:20 +0100

    add some documentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
+"""
+    Python Script Use Fabric Library to generate a achrive file
+"""
 from fabric.api import local
 from datetime import datetime
 from os import makedirs, path
 
 def do_pack():
+    """
+        Function Generate tgz archive file 
+    """
     try:
         if not path.exists('/web_static/versions'):
             makedirs('/web_static/versions')
