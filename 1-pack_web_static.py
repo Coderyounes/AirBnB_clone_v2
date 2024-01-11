a12587c446fc7707249736cf2d26f6421e4dd2a1
CoderYounes <younesanas2021@gmail.com>
2024-01-11 20:11:18 +0100

    fix some pycodestyle issue

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,17 +6,18 @@ from fabric.api import local
 from datetime import datetime
 from os import makedirs, path
 
+
 def do_pack():
     """
-        Function Generate tgz archive file 
+        Function Generate tgz archive file
     """
     try:
         if not path.exists('/web_static/versions'):
             makedirs('/web_static/versions')
-        
+
         date = datetime.now()
-        formatting = date.strftime("%Y%m%d%H%M%S")
-        archive_path = "web_static/versions/web_static_{}.tgz".format(formatting)
+        f_date = date.strftime("%Y%m%d%H%M%S")
+        archive_path = "web_static/versions/web_static_{}.tgz".format(f_date)
 
         tar_result = local("tar -cvzf {} web_static/".format(archive_path))
 
@@ -24,5 +25,5 @@ def do_pack():
             return None
         else:
             return archive_path
-    except:
+    except Exception:
         return None
