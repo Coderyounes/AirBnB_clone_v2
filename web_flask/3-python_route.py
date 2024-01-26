c173bfa49fefd511ee1a8d42fbe876d7afd86060
CoderYounes <younesanas2021@gmail.com>
2024-01-27 00:25:16 +0100

    default values for the 3rd Task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Flask Code with some Basic Functions"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """
+        Flask Function retunrn Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+        Flask Function retunrn HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_fun(text):
+    """
+        function  use Flask Variables
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_fun(text):
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
