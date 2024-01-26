c26790bcf9cb9ac73da869f1d043eeea7a41b18b
CoderYounes <younesanas2021@gmail.com>
2024-01-27 00:04:57 +0100

    Flask Variable Usage

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Flask Code with some Basic Functions"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """
+        Flask Function retunrn Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """
+        Flask Function retunrn HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_fun(text):
+    """
+        function  use Flask Variables
+    """
+    return "C is %s" % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
