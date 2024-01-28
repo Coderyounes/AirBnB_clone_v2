b491f54f998a63a092a7155b11378c201e497c79
CoderYounes <younesanas2021@gmail.com>
2024-01-28 15:40:58 +0100

    add some documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""""""
+""" Flask app list all State & Close after finish"""
 from flask import Flask
 from flask import render_template
 from models import storage
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def list_state():
     """
-
+        Flask Function list all the state
     """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
@@ -17,7 +18,7 @@ def list_state():
 
 @app.teardown_appcontext
 def termination():
-    """"""
+    """ Close the ORM Sessions after finish"""
     storage.close()
 
 
